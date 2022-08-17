eed6f293cbcffad75c1bd66f3992348da9f731ff
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-08-17 00:42:11 -0300

    object oriented programming

---
+++ object oriented programming/oop.js
@@ -0,0 +1,62 @@
+// Constructors
+
+/* Constructors are functions that create new objects.
+ They define properties and behaviors that will belong
+  to the new object. Think of them as a blueprint for
+   the creation of new objects.*/
+
+   function Bird() {
+    this.name = "Albert";
+    this.color  = "blue";
+    this.numLegs = 2;
+  }
+  let blueBird = new Bird();
+
+  // New operator
+
+/* Notice that the new operator is used when calling a constructor.
+ This tells JavaScript to create a new instance of Bird called blueBird.
+  Without the new operator, this inside the constructor would not point
+   to the newly created object, giving unexpected results. Now blueBird
+    has all the properties defined inside the Bird constructor: */
+
+    blueBird.name;
+blueBird.color;
+blueBird.numLegs;
+//Just like any other object, its properties can be accessed and modified:
+
+blueBird.name = 'Elvira';
+blueBird.name;
+
+
+/* Suppose you were writing a program to keep track of hundreds or even
+ thousands of different birds in an aviary. It would take a lot of time
+  to create all the birds, then change the properties to different values
+   for every one. To more easily create different Bird objects, you can 
+   design your Bird constructor to accept parameters: */
+
+   function Bird(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+  }
+
+  /* Then pass in the values as arguments to define each unique bird into the 
+  Bird constructor: */ let cardinal = new Bird("Bruce", "red");
+  
+  /*This gives a new
+   instance of Bird with name and color properties set to Bruce and red,
+    respectively. The numLegs property is still set to 2. The cardinal has
+     these properties: */
+
+cardinal.name
+cardinal.color
+cardinal.numLegs
+/*The constructor is more flexible. It's now possible to define the properties 
+for each Bird at the time it is created, which is one way that JavaScript 
+constructors are so useful. They group objects together based on shared 
+characteristics and behavior and define a blueprint that automates their creation. */
+
+
+// instanceof method = check if a instance belongs to the constructor, returning true or false 
+
--- practice.js
+++ practice.js
@@ -30,3 +30,61 @@ function findLongestWordLength(str) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+// Repeat a given string
+
+function repeatStringNumTimes(str, num) {
+  if (num > 0 ) {
+    return str * num
+  } else {
+    return '';
+  }
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+
+
+
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+  num = arr[i];
+  func(num) === true ? num : undefined
+  num++
+  
+ 
+ 
+  }}
+  
+
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+
+
+
+
+function frankenSplice(arr1, arr2, n) {
+  let newArray = arr2.slice()
+  let arr = []
+  for (let i = 0; i < arr1.length; i++) {
+    arr = arr1[i]
+  }
+  
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+function diffArray(arr1, arr2) {
+  
+  const mergedArray = arr1.concat(arr2)
+  const newArr  = [...new Set(mergedArray)]
+  
+
+  
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
