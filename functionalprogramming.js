ce59c3b5036bb3944bdce5ff9871c7383afd2d69
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-08-28 19:11:41 -0300

    functional programming

---
+++ functionalprogramming.js
@@ -0,0 +1,22 @@
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookName) {
+
+ let alteredArr =  bookList.push(bookName);
+  return alteredArr;
+  console.log(bookList)
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+    }
+}
