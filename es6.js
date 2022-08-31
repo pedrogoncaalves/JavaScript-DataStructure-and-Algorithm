3882d012e3a75225787bf9fe7cdb3530317fabe7
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-08-31 14:26:35 -0300

    es6

---
+++ es6.js
@@ -0,0 +1,27 @@
+/* When you declare a variable with the var keyword,
+ it is declared globally, or locally if declared
+  inside a function.
+
+The let keyword behaves similarly, but with some extra features.
+ When you declare a variable with the let keyword inside a block,
+  statement, or expression, its scope is limited to that block,
+   statement, or expression. */
+
+   
+
+
+   // Rest parameters
+
+   /*In order to help us create more flexible functions,
+    ES6 introduces the rest parameter for function parameters.
+     With the rest parameter, you can create functions that
+      take a variable number of arguments. These arguments 
+      are stored in an array that can be accessed later
+       from inside the function.*/
+ 
+   function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+  }
+  console.log(howMany(0, 1, 2));
+  console.log(howMany("string", null, [1, 2, 3], { }));
+
