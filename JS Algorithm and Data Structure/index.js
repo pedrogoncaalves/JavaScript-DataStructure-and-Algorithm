a07b2e1bf97021c3b1ff0078d1e5fca37321aea0
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-10 00:11:46 -0300

    manipulating strings

---
+++ JS Algorithm and Data Structure/index.js
@@ -0,0 +1,10 @@
+// Intermediate algorithm scripting
+
+function sentensify(str) {
+    // Only change code below this line
+  return str.split('-').join(" ")
+  
+    // Only change code above this line
+  }
+  
+  console.log(sentensify("May-the-force-be-with-you"));
